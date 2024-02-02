f650a23f62220904d76d6046fb7d142d0ffbf172
LucasDWWM <lucaslipari06@gmail.com>
2024-02-02 15:14:44 +0100

    ajout cards

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // Déclaration d'une classe Produit
 class Produit {
-  constructor(
-    nom,
-    quantite,
-    prixAchatHT,
-    prixVenteHT,
-    type,
-    degreAlcool = null
-  ) {
-    this.nom = nom;
-    this.quantite = +quantite; // Utilisation de l'opérateur '+' pour convertir en nombre
-    this.prixAchatHT = +prixAchatHT;
-    this.prixVenteHT = +prixVenteHT;
-    this.type = type;
-    this.degreAlcool = degreAlcool !== null ? +degreAlcool : null; // Conversion en nombre si degreAlcool n'est pas null
-    this.calculerMargeHT();
-    this.calculerPrixVenteTTC();
-  }
+    constructor(
+        nom,
+        quantite,
+        prixAchatHT,
+        prixVenteHT,
+        type,
+        degreAlcool = null
+    ) {
+        this.nom = nom;
+        this.quantite = +quantite; // Utilisation de l'opérateur '+' pour convertir en nombre
+        this.prixAchatHT = +prixAchatHT;
+        this.prixVenteHT = +prixVenteHT;
+        this.type = type;
+        this.degreAlcool = degreAlcool !== null ? +degreAlcool : null; // Conversion en nombre si degreAlcool n'est pas null
+        this.calculerMargeHT();
+        this.calculerPrixVenteTTC();
+    }
 
-  calculerMargeHT() {
-    this.margeHT = this.prixVenteHT - this.prixAchatHT;
-  }
+    calculerMargeHT() {
+        this.margeHT = this.prixVenteHT - this.prixAchatHT;
+    }
 
-  calculerPrixVenteTTC() {
-    const tauxTVA = 0.2;
-    this.prixVenteTTC = this.prixVenteHT * (1 + tauxTVA);
-  }
+    calculerPrixVenteTTC() {
+        const tauxTVA = 0.2;
+        this.prixVenteTTC = this.prixVenteHT * (1 + tauxTVA);
+    }
 }
 
 // Liste de produits
@@ -33,66 +33,63 @@ let listeProduits = [];
 
 // Fonction pour ajouter un produit
 function ajouterProduit() {
-  let nom = document.getElementById("productName").value;
-  let quantite = document.getElementById("productQuantity").value;
-  let prixAchatHT = document.getElementById("productPrixAchat").value;
-  let prixVenteHT = document.getElementById("productPrixVente").value;
-  let type = document.getElementById("productType").value;
-  let degreAlcool = document.getElementById("productDegreAlcool").value || null;
+    let nom = document.getElementById("productName").value;
+    let quantite = document.getElementById("productQuantity").value;
+    let prixAchatHT = document.getElementById("productPrixAchat").value;
+    let prixVenteHT = document.getElementById("productPrixVente").value;
+    let type = document.getElementById("productType").value;
+    let degreAlcool = document.getElementById("productDegreAlcool").value || null;
 
-  let nouveauProduit = new Produit(
-    nom,
-    quantite,
-    prixAchatHT,
-    prixVenteHT,
-    type,
-    degreAlcool
-  );
-  listeProduits.push(nouveauProduit);
+    let nouveauProduit = new Produit(
+        nom,
+        quantite,
+        prixAchatHT,
+        prixVenteHT,
+        type,
+        degreAlcool
+    );
+    listeProduits.push(nouveauProduit);
 
-  afficherListeProduits();
+    afficherListeProduits();
 }
 
 // Fonction pour afficher la liste des produits
 function afficherListeProduits() {
-  let listeProduitsDiv = document.getElementById("listeProduits");
-  listeProduitsDiv.innerHTML = "";
+    let listeProduitsDiv = document.getElementById("listeProduits");
+    listeProduitsDiv.innerHTML = "";
 
-  listeProduits.forEach((produit) => {
-    let produitDiv = document.createElement("div");
-    produitDiv.innerHTML = `
+    listeProduits.forEach((produit) => {
+        let produitDiv = document.createElement("div");
+        produitDiv.innerHTML = `
               <p>${produit.nom}</p>
               <p>Quantité: ${produit.quantite}</p>
               <p>Prix d'achat HT: ${produit.prixAchatHT}</p>
               <p>Prix de vente HT: ${produit.prixVenteHT}</p>
               <p>Type: ${produit.type}</p>
-              <p>Degré d'alcool: ${
-                produit.degreAlcool !== null ? produit.degreAlcool : "N/A"
-              }</p>
+              <p>Degré d'alcool: ${produit.degreAlcool !== null ? produit.degreAlcool : "N/A"
+            }</p>
               <p>Marge HT: ${produit.margeHT}</p>
-              <button onclick="modifierQuantite('${
-                produit.nom
-              }', -1)">Décrémenter Stock</button>
-              <button onclick="modifierQuantite('${
-                produit.nom
-              }', 1)">Incrémenter Stock</button> <hr>`;
+              <button onclick="modifierQuantite('${produit.nom
+            }', -1)">Décrémenter Stock</button>
+              <button onclick="modifierQuantite('${produit.nom
+            }', 1)">Incrémenter Stock</button> <hr>`;
 
-    listeProduitsDiv.appendChild(produitDiv);
-  });
+        listeProduitsDiv.appendChild(produitDiv);
+    });
 }
 
 // Fonction pour modifier la quantité d'un produit
 function modifierQuantite(nomProduit, increment) {
-  let produit = listeProduits.find((p) => p.nom === nomProduit);
+    let produit = listeProduits.find((p) => p.nom === nomProduit);
 
-  if (produit) {
-    if (produit.quantite + increment >= 0) {
-      produit.quantite += increment;
-      afficherListeProduits();
-    } else {
-      alert("Le stock ne peut pas être négatif.");
+    if (produit) {
+        if (produit.quantite + increment >= 0) {
+            produit.quantite += increment;
+            afficherListeProduits();
+        } else {
+            alert("Le stock ne peut pas être négatif.");
+        }
     }
-  }
 }
 
 // Appel initial pour afficher la liste des produits au chargement de la page

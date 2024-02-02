a859d1f86c10f4a8f8fa8280b2f6335c7059ee17
nicolasJiv <nicolas.jivan@gmail.com>
2024-02-02 16:12:26 +0100

    style.css

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Déclaration d'une classe Produit
 class Produit {
-    constructor(
-        nom,
-        quantite,
-        prixAchatHT,
-        prixVenteHT,
-        type,
-        degreAlcool = null
-    ) {
-        this.nom = nom;
-        this.quantite = +quantite; // Utilisation de l'opérateur '+' pour convertir en nombre
-        this.prixAchatHT = +prixAchatHT;
-        this.prixVenteHT = +prixVenteHT;
-        this.type = type;
-        this.degreAlcool = degreAlcool !== null ? +degreAlcool : null; // Conversion en nombre si degreAlcool n'est pas null
-        this.calculerMargeHT();
-        this.calculerPrixVenteTTC();
-    }
+  constructor(
+    nom,
+    quantite,
+    prixAchatHT,
+    prixVenteHT,
+    type,
+    degreAlcool = null
+  ) {
+    this.nom = nom;
+    this.quantite = +quantite; // Utilisation de l'opérateur '+' pour convertir en nombre
+    this.prixAchatHT = +prixAchatHT;
+    this.prixVenteHT = +prixVenteHT;
+    this.type = type;
+    this.degreAlcool = degreAlcool !== null ? +degreAlcool : null; // Conversion en nombre si degreAlcool n'est pas null
+    this.calculerMargeHT();
+    this.calculerPrixVenteTTC();
+  }
 
-    calculerMargeHT() {
-        this.margeHT = this.prixVenteHT - this.prixAchatHT;
-    }
+  calculerMargeHT() {
+    this.margeHT = this.prixVenteHT - this.prixAchatHT;
+  }
 
   calculerPrixVenteTTC() {
     const tauxTVA = 0.2;
@@ -37,30 +37,30 @@ let listeProduits = [];
 
 // Fonction pour ajouter un produit
 function ajouterProduit() {
-    let nom = document.getElementById("productName").value;
-    let quantite = document.getElementById("productQuantity").value;
-    let prixAchatHT = document.getElementById("productPrixAchat").value;
-    let prixVenteHT = document.getElementById("productPrixVente").value;
-    let type = document.getElementById("productType").value;
-    let degreAlcool = document.getElementById("productDegreAlcool").value || null;
+  let nom = document.getElementById("productName").value;
+  let quantite = document.getElementById("productQuantity").value;
+  let prixAchatHT = document.getElementById("productPrixAchat").value;
+  let prixVenteHT = document.getElementById("productPrixVente").value;
+  let type = document.getElementById("productType").value;
+  let degreAlcool = document.getElementById("productDegreAlcool").value || null;
 
-    let nouveauProduit = new Produit(
-        nom,
-        quantite,
-        prixAchatHT,
-        prixVenteHT,
-        type,
-        degreAlcool
-    );
-    listeProduits.push(nouveauProduit);
+  let nouveauProduit = new Produit(
+    nom,
+    quantite,
+    prixAchatHT,
+    prixVenteHT,
+    type,
+    degreAlcool
+  );
+  listeProduits.push(nouveauProduit);
 
-    afficherListeProduits();
+  afficherListeProduits();
 }
 
 // Fonction pour afficher la liste des produits
 function afficherListeProduits() {
-    let listeProduitsDiv = document.getElementById("listeProduits");
-    listeProduitsDiv.innerHTML = "";
+  let listeProduitsDiv = document.getElementById("listeProduits");
+  listeProduitsDiv.innerHTML = "";
 
   listeProduits.forEach((produit) => {
     let produitDiv = document.createElement("div");
@@ -97,22 +97,22 @@ function afficherListeProduits() {
           </div>
           <hr>`;
 
-        listeProduitsDiv.appendChild(produitDiv);
-    });
+    listeProduitsDiv.appendChild(produitDiv);
+  });
 }
 
 // Fonction pour modifier la quantité d'un produit
 function modifierQuantite(nomProduit, increment) {
-    let produit = listeProduits.find((p) => p.nom === nomProduit);
+  let produit = listeProduits.find((p) => p.nom === nomProduit);
 
-    if (produit) {
-        if (produit.quantite + increment >= 0) {
-            produit.quantite += increment;
-            afficherListeProduits();
-        } else {
-            alert("Le stock ne peut pas être négatif.");
-        }
+  if (produit) {
+    if (produit.quantite + increment >= 0) {
+      produit.quantite += increment;
+      afficherListeProduits();
+    } else {
+      alert("Le stock ne peut pas être négatif.");
     }
+  }
 }
 
 // Appel initial pour afficher la liste des produits au chargement de la page
